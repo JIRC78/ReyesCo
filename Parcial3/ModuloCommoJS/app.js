e9bea440962f8d60bb9f1785c0800c860ff8e282
JIRC78 <jirc2@outlook.com>
2022-11-09 12:38:59 -0600

    cambiovaca

---
+++ Parcial3/ModuloCommoJS/app.js
@@ -0,0 +1,11 @@
+const modulo = require("./modulocmm");
+
+console.log(modulo.cuadrado(2));
+
+var cowsay = require("cowsay");
+
+console.log(cowsay.say({
+    text : "I'm a moooodule",
+    e : "oO",
+    T : "U "
+}));
