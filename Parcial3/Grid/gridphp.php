516c8e3241795bee496948cf2b785c4a27e866b8
JIRC78 <jirc2@outlook.com>
2022-11-26 16:03:05 -0600

    cambiogrid

---
+++ Parcial3/Grid/gridphp.php
@@ -0,0 +1,30 @@
+<?php
+
+$servidor = "localhost";
+$basedatos = "baselibro";
+$usuario = "root";
+$password = "";
+
+$con  = mysqli_connect($servidor, $usuario, $password, $basedatos) or die("no se pudo conectar a localhost");
+$consulta = "select * from libro";
+
+$registros = mysqli_query($con, $consulta) or die ("problemas con el select");
+
+// while ($reg = mysqli_fetch_array($registros, MYSQLI_ASSOC)) {
+//     # code...
+//     printf($reg['nombre'].' '.$reg['apPaterno'].'<br>');
+
+// }
+
+// while ($reg = mysqli_fetch_array($registros, MYSQLI_NUM)) {
+//     # code...
+//     printf($reg[1].' '.$reg[2].'<br>');
+// }
+
+$result = mysqli_fetch_all($registros,MYSQLI_ASSOC);
+
+mysqli_close($con);
+echo json_encode($result);
+
+
+?>
