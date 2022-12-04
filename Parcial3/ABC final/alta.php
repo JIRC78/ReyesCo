78dc0862a962429642a0df735e45ab5cd23a833d
JIRC78 <jirc2@outlook.com>
2022-12-04 13:14:24 -0600

    cambiopdf

---
+++ Parcial3/ABC final/alta.php
@@ -0,0 +1,42 @@
+<?php
+$id=$_POST['idLibro'];
+$NOMBRE=$_POST['idNombre'];
+$GENERO=$_POST['idGenero'];
+$SUBGEN=$_POST['idsubGenero'];
+$AUTOR=$_POST['idAutor'];
+$IDIOMA=$_POST['idIdioma'];
+$EDITORIAL=$_POST['idEditorial'];
+$AÑO=$_POST['idAño'];
+
+
+$servidor='localhost';
+$basedatos='baselibro';
+$usuario='root';
+$password='';
+//$port='3306';
+
+$idLibro=$_POST['par1'];
+try {
+    $con = new PDO($servidor,$usuario,$password,$basedatos);
+} catch(PDOException $e) {
+    echo "Error de conexion a la base de datos";
+    echo $e->getMessage();
+    exit();
+}
+
+//$con->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+
+try {
+$consultaSql = " Insert into libro (idLibro, Nombre, Genero, SubGenero, Autor, Idioma, Editorial, Año) VALUES ('$id',
+'$NOMBRE','$GENERO','$SUBGEN','$AUTOR','$IDIOMA','$EDITORIAL','$AÑO')";
+$consulta = $con -> prepare($consultaSql);
+$consulta -> execute();
+$consulta->closeCursor();
+echo "SE HAN INTRODUCIDO LOS DATOS DEL NUEVO REGISTRO CORRECTAMENTE";
+} catch(PDOException $e) {
+    echo "Error de consulta a la base de datos";
+    echo $e->getMessage();
+}
+
+
+?>
